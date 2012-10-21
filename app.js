951e17e947f5d312dbefad5ef13b5948f739cf87
Matthew Murphy <nonplayablecharacter@hotmail.com>
2012-10-20 20:29:38 -0400

    socket.io and mongodb is setup

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
-
-/**
- * Module dependencies.
- */
-
-var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
-  , http = require('http')
-  , path = require('path');
+var express = require('express'), http = require('http'), path = require('path');
+  
+var mongo = require('mongodb'), Server = mongo.Server, Db = mongo.Db;
+var serverOptions = {
+  'auto_reconnect': true,
+  'poolSize': 1
+};
+var server = new Server("alex.mongohq.com", 10011, serverOptions);
+var db = new Db("chat-io", server);
 
 var app = express();
 
@@ -27,9 +26,45 @@ app.configure('development', function(){
   app.use(express.errorHandler());
 });
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+app.get('/', function(req, res){
+    res.render('index', { title: 'chat.io' });
+});
 
-http.createServer(app).listen(app.get('port'), function(){
+var appServer = http.createServer(app);
+appServer.listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
 });
+
+var io = require('socket.io').listen(appServer);
+io.configure(function () {
+    // io.set('transports', ['websocket', 'flashsocket', 'xhr-polling']);
+    io.set('transports', ['xhr-polling']);
+});
+io.sockets.on('connection', function (socket) {
+  socket.on("login", function(credentials) {
+    db.open(function(err, db) {
+        db.authenticate("chat-io-admin", "pass", function(err, result) {
+            if(!err) {
+                db.collection("users", function(err, collection) {
+                    if(!err) {
+                        collection.find({username:"mashumafi"}).toArray(function(err, items) {
+                            if(err == null) {
+                                socket.emit('login', items[0]);
+                            } else {
+                                res.send(err);
+                            }
+                            db.close();
+                        });
+                    } else {
+                        res.send(err);
+                    }
+                });
+            } else {
+                res.send(err);
+            }
+        });
+    });
+  });
+});
+
+
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,23 @@
-
-/*
- * GET home page.
- */
-
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+    db.open(function(err, db) {
+        db.authenticate("chat-io-admin", "pass", function(err, result) {
+            if(!err) {
+                db.collection("users", function(err, collection) {
+                    if(!err) {
+                        collection.find({username:"mashumafi"}).toArray(function(err, items) {
+                            if(err == null) {
+                                res.render('index', { title: 'Express', result: items[0].email });
+                            } else {
+                                res.send(err);
+                            }
+                        });
+                    } else {
+                        res.send(err);
+                    }
+                });
+            } else {
+                res.send(err);
+            }
+        });
+    });
 };
